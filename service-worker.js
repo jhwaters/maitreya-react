a3ccc534794d2181a5b065980323f3db4927d10a
JW <>
2019-01-24 19:27:00 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/maitreya-react/precache-manifest.8e4dc7b5328388f3904d96eeffd42ebc.js"
+  "/maitreya-react/precache-manifest.a84d1d4ab64e1e01c0d066c2a66c0a4e.js"
 );
 
 workbox.clientsClaim();
